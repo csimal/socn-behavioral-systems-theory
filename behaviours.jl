3c6798f20c9d664ff8c6633ec7aa5e25c68c685e
csimal <cedric.simal@unamur.be>
2022-11-17 17:12:11 +0100

    Add notebook

--- behaviours.jl
+++ behaviours.jl
@@ -3,17 +3,36 @@ using LinearAlgebra
 
 include("utils.jl")
 
+"""
+    ss2BT_modelbased(sys,T)
 
-function ss2BT(A,B,C,D,Π, T)
+Compute a basis of the behaviour ℬ_T.
+"""
+function ss2BT_modelbased(sys,Π,T)
+    A,B,C,D = sys.A,sys.B,sys.C,sys.D
     n = size(A,1)
     m = size(B,2)
     p = size(C,1)
     q = m + p
-    Q = zeros(q*T, m*T+n)
-    for i in 1:(m*T)
-        Q[i,i] = 1.0
+    M = [zeros(m*T,n) I; zeros(p*T, n+m*T)]
+    O = copy(C) 
+    M[(m*T+1):(m*T+p), 1:n] .= C
+    for t in 1:T
+        rows = m*T + (t-1)*p .+ (1:p)
+        cols = n + (t-1)*m .+ (1:m)
+        M[rows,cols] .= D
     end
-
+    for t in 2:T
+        rows = m*T + (t-1)*p .+ (1:p)
+        M[rows,1:n] .= O
+        for s in t:T
+            rows_ = m*T + (s-1)*p .+ (1:p)
+            cols_ = n + (s-t)*m .+ (1:m)
+            M[rows_,cols_] .= O*B
+        end
+        O *= A
+    end
+    return M
 end
 
 """
@@ -36,10 +55,16 @@ end
 """
     ss2BT_hankel(sys, T, L)
 
-Compute an orthonormal basis of ℬ_L using the Hankel matrix of a random trajectory.
+Compute an orthonormal basis of ℬ_L using the Hankel matrix of a random trajectory of length `T``.
 """
 function ss2BT_hankel(sys,T,L)
     w = random_trajectory(sys,T)
     ℋ = hankel_matrix(w,L)
     return extract_basis(ℋ)
 end
+
+function ss2BT_hankel(sys, T)
+    n,m,_ = sizes(sys)
+    return ss2BT_hankel(sys, (m+1)*T+n, T)
+end
+    
